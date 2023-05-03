0d1702152505b75bdcc08f06a1556eba0f7a73e0
JorgeLuix <tec.jorge.29@gmai.com>
2023-05-03 17:47:41 +0200

    added pagina2php and stili

--- pagina2.php
+++ pagina2.php
@@ -4,7 +4,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style/styles.css">
     <title>Elaborazione dati</title>
 </head>
+<body>
 
+    <?php
+    // Recupero i dati dal form
+    $paragrafo = $_POST["paragrafo"];
+    $parola = $_POST["parola"];
+
+    // Stampo il paragrafo e la sua lunghezza
+    echo '<div class="container">';
+    echo '<div class="row">';
+    echo '<div class="col-md-12">';
+    echo '<h5>Paragrafo originale:</h5>';
+    echo "<p>$paragrafo</p>";
+    echo '<p class="fw-semibold">Lunghezza del paragrafo: ' . strlen($paragrafo) . '</p>';
+
+    // Censuro la parola
+    $paragrafo_censurato = str_replace($parola, '<span class="censurato">***</span>', $paragrafo);
+
+    // Stampo il paragrafo censurato e la sua lunghezza
+    echo '<h5>Paragrafo censurato:</h5>';
+    echo "<p>$paragrafo_censurato</p>";
+    echo '<p class="fw-semibold">Lunghezza del paragrafo censurato: ' . strlen($paragrafo_censurato) . '</p>';
+    echo '</div>';
+    echo '</div>';
+    echo '</div>';
+?>
+</body>
 </html>
